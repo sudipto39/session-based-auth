b4ed7ffdefebb4c0ddb7f0da9ee03344baa5d165
123sudipto <sudipto002gayen@gmail.com>
2025-12-29 18:47:33 +0530

    DB is connected

---
+++ config/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      autoIndex: false, // better for production
+    });
+
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1); // fail fast
+  }
+};
+
+export default connectDB;
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import dotenv from "dotenv";
-dotenv.config();
+dotenv.config({path:"./.env"});
 
 import app from "./app.js";
 import connectDB from "./config/db.js";
 import redisClient from "./config/Redis.js";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 const startServer = async () => {
   try {
